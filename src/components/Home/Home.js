b4d56c2ab634dd200e496d1aa4fe8b241c691f08
Al-Amin931761 <alamin931761@gmail.com>
2022-04-04 00:05:04 +0600

    header part

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet</h1>
+        </div>
+    );
+};
+
+export default Home; <h2>This is home</h2>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-rose-900'>404</h1>
+            <h2 className='text-3xl'>This page is not available</h2>
+        </div>
+    );
+};
+
+export default NotFound;
